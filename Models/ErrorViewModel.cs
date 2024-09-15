9fd6322e3f7a3f1e3aa47dc564dd19e27e128220
FabijanPC <FabijanPC@FABIJAN>
2024-09-15 11:12:42 +0200

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MVCstiburek.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
